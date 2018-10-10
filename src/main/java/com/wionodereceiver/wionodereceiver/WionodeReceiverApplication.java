20a9d6af4c375937a7d18bbf2d63f015f599ab71
edwin-gonzales-n <edwin.gonzales.n@gmail.com>
2018-10-09 23:57:14 -0500

    initializing project env

---
+++ src/main/java/com/wionodereceiver/wionodereceiver/WionodeReceiverApplication.java
@@ -0,0 +1,12 @@
+package com.wionodereceiver.wionodereceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WionodeReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WionodeReceiverApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wionodereceiver/wionodereceiver/WionodeReceiverApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wionodereceiver.wionodereceiver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WionodeReceiverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
